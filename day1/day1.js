45331191767ca7f93829efd5d1321392956f140f
Thomas Robinson <git@tomr.me>
2022-12-01 18:32:14 +0000

    day 1 pt 2

--- day1/day1.js
+++ day1/day1.js
@@ -3,11 +3,11 @@ import {readFileSync} from 'node:fs';
 // Read Input file
 const input = readFileSync('input.txt', { encoding: 'utf8', flag: 'r' });
 
+
+// Find what each elf is carrying
 let elves = [];
 let i = 0;
 
-
-// Find what each elf is carrying
 for (let line of input.split('\n')) {
     if (line.length <= 1) i++;
 
@@ -25,3 +25,10 @@ for(let elf of elves) {
 }
 
 console.log(currentTop);
+
+
+// Part Two: Adding top three values
+let elvesSorted = elves.sort((a, b) => b - a); // this feels like cheating
+
+let topThree = elvesSorted[0] + elvesSorted[1] + elvesSorted[2];
+console.log(topThree);
