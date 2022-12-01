44205db73ec4c52e62c782d9d3f185518e24ad35
Thomas Robinson <git@tomr.me>
2022-12-01 18:36:53 +0000

    code formatting

--- day1/day1.js
+++ day1/day1.js
@@ -1,28 +1,27 @@
-import {readFileSync} from 'node:fs';
+import { readFileSync } from "node:fs";
 
 // Read Input file
-const input = readFileSync('input.txt', { encoding: 'utf8', flag: 'r' });
+const input = readFileSync("input.txt", { encoding: "utf8", flag: "r" });
 
 
 // Find what each elf is carrying
 let elves = [];
 let i = 0;
 
-for (let line of input.split('\n')) {
-    if (line.length <= 1) i++;
+for (let line of input.split("\n")) {
+  if (line.length <= 1) i++;
 
-    if(!elves[i]) elves[i] = 0;
+  if (!elves[i]) elves[i] = 0;
 
-    elves[i] += parseInt(line);
+  elves[i] += parseInt(line);
 }
-
 
 // Find the biggest
 let currentTop = 0;
 
-for(let elf of elves) {
-    if(elf > currentTop) currentTop = elf;
-}
+elves.forEach(e => {
+    if (e>currentTop) currentTop = e 
+});
 
 console.log(currentTop);
 
@@ -30,5 +29,6 @@ console.log(currentTop);
 // Part Two: Adding top three values
 let elvesSorted = elves.sort((a, b) => b - a); // this feels like cheating
 
-let topThree = elvesSorted[0] + elvesSorted[1] + elvesSorted[2];
+let topThree = elvesSorted[0] + elvesSorted[1] + elvesSorted[2]; // this is the best code i have ever written
+
 console.log(topThree);
