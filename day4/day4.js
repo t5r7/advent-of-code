7e6346abae1083e500316d051376d56cdd7561c2
Thomas Robinson <git@tomr.me>
2022-12-04 06:20:16 +0000

    oh it wasn't broken i am just silly

--- day4/day4.js
+++ day4/day4.js
@@ -4,44 +4,35 @@ import { readFileSync } from "node:fs";
 const input = readFileSync("input.txt", { encoding: "utf8", flag: "r" });
 
 let count = 0;
+
 for(const group of input.split("\n")) {
     if(!group) continue; // empty group
-
-    let contained = false;
     
     const firstElfRange = group.split(',')[0];
     const secondElfRange = group.split(',')[1];
 
-    const firstElfLowest = firstElfRange.split('-')[0];
-    const firstElfHighest = firstElfRange.split('-')[1];
+    const firstElfLowest = parseInt(firstElfRange.split('-')[0]);
+    const firstElfHighest = parseInt(firstElfRange.split('-')[1]);
 
-    const secondElfLowest = secondElfRange.split('-')[0];
-    const secondElfHighest = secondElfRange.split('-')[1];
+    const secondElfLowest = parseInt(secondElfRange.split('-')[0]);
+    const secondElfHighest = parseInt(secondElfRange.split('-')[1]);
 
-    if (secondElfLowest >= firstElfLowest && secondElfHighest <= firstElfHighest) contained = true;
-    if (firstElfLowest >= secondElfLowest && firstElfHighest <= secondElfHighest) contained = true;
-
-    // console.log(group, contained);
-    if(contained) {
-        console.log(group, getBetweenRange(firstElfRange), getBetweenRange(secondElfRange));
+    // if the second elf's sections are within the first elf's
+    // as in the example from aoc below:
+    // .2345678.  2 - 8
+    // ..34567..  3 - 7
+    // second elf's lowest is greater than or eq to first elf's lowest
+    // second elf's highest is less than or eq to first elf's highest
+    if (secondElfLowest >= firstElfLowest && secondElfHighest <= firstElfHighest) {
+        count++;
+        continue;
     }
-    
-    if (contained) count++;
+
+    // inverse of above; if first elf is within second elf
+    if (firstElfLowest >= secondElfLowest && firstElfHighest <= secondElfHighest) {
+        count++;
+        continue;
+    }
 }
 
 console.log(count);
-
-
-// turns out this wasnt required oops
-function getBetweenRange(range) {
-    let nums = [];
-
-    const from = parseInt(range.split('-')[0]);
-    const to = parseInt(range.split('-')[1]);
-
-    for(let i = from; i <= to; i++) {
-        nums.push(i);
-    }
-
-    return nums.sort((a, b) => { return a - b }); // sort from smallest to largest and return
-}
