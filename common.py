84fbc79bcdf4800459d801ea7a77e2114e76235e
Thomas Robinson <git@tomr.me>
2023-12-01 22:48:47 +0000

    day one done

--- common.py
+++ common.py
@@ -2,12 +2,13 @@ import os
 
 FOLDER = "inputs"
 
-def getDayInput(day, test=False):
+def getDayInput(day, part=1, test=False):
 	"""
 	Reads the input file for the given day and returns its contents as a list of strings.
 
 	Parameters:
 		- day (int): The day for which to read the input file.
+		- part (int): Part 1 or 2 of the challenge. Defaults to 1.
 		- test (bool): Whether to read the test input file for that day. Defaults to False.
 
 	Returns:
@@ -15,11 +16,14 @@ def getDayInput(day, test=False):
 	"""
 
 	if test:
-		fileName = f"day{str(day).zfill(2)}-test.txt"
+		fileName = f"day{str(day).zfill(2)}-test{part}.txt"
+
 	else:
 		fileName = f"day{str(day).zfill(2)}.txt"
 
 	filePath = os.path.join(FOLDER, fileName)
 
 	with open(filePath, "r") as f:
-		return f.read().splitlines()
+		lines = f.read().splitlines()
+		lines = [line for line in lines if line]  # remove blank lines
+		return lines
--- day01.py
+++ day01.py
@@ -1,5 +1,71 @@
 from common import *
 
-print(getDayInput(1, True))
+NUMBERS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+				  
+def part1():
+	lines = getDayInput(1, 1)
 
-print(getDayInput(1, False))
+	digits = []
+
+	for line in lines:
+		lineDigits = []
+
+		for char in line:
+			if char.isdigit():
+				lineDigits.append(int(char))
+			
+		# "calibration value" of this line is the first and last digits, concatenated
+		calibValue = int(f"{lineDigits[0]}{lineDigits[-1]}")
+
+		digits.append(calibValue)
+
+	return sum(digits)
+
+
+def part2():
+	lines = getDayInput(1, 2, False)
+
+	digits = []
+
+	for line in lines:		
+		# print(f"\n{line}")
+		chars = list(line)
+		
+		# check for any spelled out numbers, many times
+		for _ in range(0, len(chars)*2):
+			for i in range(len(NUMBERS)):
+				if NUMBERS[i] in "".join(chars):
+					# we've found a number, where was it?
+					index = "".join(chars).index(NUMBERS[i])
+
+					# print(f"found an {NUMBERS[i]} ({i+1}) at {index}")
+
+					# replace second letter of number with digit
+					# cannot do the first letter because of overlapping lettes;
+					# eg: nineight, twone, etc (fuck this)
+					chars[index+1] = str(i+1)
+
+					# print(chars)
+
+		
+		# remove non-digits from the list
+		lineDigits = []
+		
+		for char in chars:
+			if char.isdigit():
+				lineDigits.append(int(char))
+
+		# print(lineDigits)
+
+		# "calibration value" of this line is the first and last digits, concatenated
+		calibValue = int(f"{lineDigits[0]}{lineDigits[-1]}")
+
+		# print(calibValue)
+
+		digits.append(calibValue)
+
+	return sum(digits)
+
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
