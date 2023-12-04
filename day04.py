002397125b5ace99eaa40f9b59f9212d115761f0
Thomas Robinson <git@tomr.me>
2023-12-04 14:23:12 +0000

    day 4 pt2!!

--- day04.py
+++ day04.py
@@ -1,33 +1,34 @@
 from common import *
 
+def getMatchCount(card):
+	card = card.split(": ")
+	card.pop(0)
+
+	card = "".join(card) # join the list back into a string
+
+	halves = card.split("|")
+
+	winningNums = halves[0].split(" ")
+	ourNums = halves[1].split(" ")
+
+	# remove empty things
+	winningNums = [x for x in winningNums if x != ' ' and x != '']
+	ourNums = [x for x in ourNums if x != ' ' and x != '']
+	
+	matches = 0
+	for num in winningNums:
+		if num in ourNums:
+			matches += 1
+
+	return matches
+
 def part1():
 	lines = getDayInput(4, 1)
 
 	totalPoints = []
 
 	for card in lines:
-		# remove "card #: "
-		card = card.split(": ")
-		card.pop(0)
-
-		card = "".join(card) # join the list back into a string
-
-		halves = card.split("|")
-
-		winningNums = halves[0].split(" ")
-		ourNums = halves[1].split(" ")
-
-		# remove empty things
-		winningNums = [x for x in winningNums if x != ' ' and x != '']
-		ourNums = [x for x in ourNums if x != ' ' and x != '']
-		
-		# testMatched = []
-
-		matches = 0
-		for num in winningNums:
-			if num in ourNums:
-				matches += 1
-				# testMatched.append(num)
+		matches = getMatchCount(card)
 
 		if matches == 0:
 			score = 0
@@ -44,4 +45,28 @@ def part1():
 
 	return sum(totalPoints)
 
+def part2():
+	lines = getDayInput(4, 2)
+	scratchcards = []
+
+	for card in lines:
+		scratchcards.append({
+			"matches": getMatchCount(card),
+			"count": 1	
+		})
+
+	for i in range(0, len(scratchcards)):
+		card = scratchcards[i]
+
+		for _ in range(0, card["count"]):
+			for j in range(i+1, i+1+card["matches"]):
+				scratchcards[j]["count"] += 1
+
+	total = 0
+	for card in scratchcards:
+		total += card["count"]
+
+	return total
+
 print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
