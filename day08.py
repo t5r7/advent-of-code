04590be7990224ef2f39d855ee807f4b84b72589
Thomas Robinson <git@tomr.me>
2023-12-10 19:22:52 +0000

    call day8 part2() in print like others

--- day08.py
+++ day08.py
@@ -115,6 +115,5 @@ print(f"Part 1: {part1()}")
 print(f"Time: {round(time.time() - startTime, 2)} seconds")
 
 startTime = time.time()
-pt2 = part2()
-print(f"Part 2: {pt2}")
+print(f"Part 2: {part2()}")
 print(f"Time: {round(time.time() - startTime, 2)} seconds")
