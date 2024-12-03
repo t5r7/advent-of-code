825259a7f9e1851de564f8aada729b32abe55260
Thomas Robinson <git@tomr.me>
2024-12-03 02:34:08 +0000

    (day 2) use pt2 func in pt1

--- day02.py
+++ day02.py
@@ -9,43 +9,8 @@ def part_one():
 			continue
 
 		line_nums = line.split(' ')
-		line_is_safe = True # assume safe until proven otherwise
-		seen_pos_diff = False
-		seen_neg_diff = False
 
-		print(line_nums)
-
-		for i in range(len(line_nums) - 1):
-			diff = int(line_nums[i]) - int(line_nums[i + 1])
-			# print(f"{line_nums[i]} - {line_nums[i + 1]} = {diff}")
-
-			# if diff is 0, not safe
-			if diff == 0:
-				# print("not safe - diff is 0")
-				line_is_safe = False
-				break
-
-			if diff > 0:
-				seen_neg_diff = True
-			elif diff < 0:
-				seen_pos_diff = True
-
-			# if we've seen both a positive and negative diff, this line is not safe
-			if seen_pos_diff and seen_neg_diff:
-				# print("not safe - both pos and neg diffs")
-				line_is_safe = False
-				break
-
-			# now convert diff to absolute value, we no longer care about +/-
-			diff = abs(diff)
-
-			# if the diff is < 1 or > 3, not safe
-			if diff < 1 or diff > 3:
-				# print("not safe - diff < 1 or > 3")
-				line_is_safe = False
-				break
-		
-		if line_is_safe:
+		if(check_levels(line_nums)):
 			safe_count += 1
 
 	return safe_count
@@ -60,7 +25,7 @@ def part_two():
 			continue
 
 		line_nums = line.split(' ')
-		if(part_two_check_levels(line_nums)):
+		if(check_levels(line_nums)):
 			safe_count += 1
 		else:
 			# see if removing any number makes it safe
@@ -68,13 +33,13 @@ def part_two():
 				# there are probably better ways to do this than brute force
 				new_line_nums = line_nums.copy()
 				new_line_nums.pop(i)
-				if(part_two_check_levels(new_line_nums)):
+				if(check_levels(new_line_nums)):
 					safe_count += 1
 					break
 
 	return safe_count
 
-def part_two_check_levels(levels):
+def check_levels(levels):
 	line_is_safe = True  # assume safe until proven otherwise
 	seen_pos_diff = False
 	seen_neg_diff = False
